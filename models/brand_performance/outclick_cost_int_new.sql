af26813c895218a81bb5e58bfde2edaa54f6ceca
dnlpankov <dnlpankov@gmail.com>
2024-05-01 20:56:27 +0300

    fixed the no running command

--- models/brand_performance/outclick_cost_int_new.sql
+++ models/brand_performance/outclick_cost_int_new.sql
@@ -9,12 +9,12 @@
         ga_campaign_name, 
         brand_name,
         unique_outclicks,
-        -- traffic_source,
+        sources.traffic_source,
         NULL as cost
     from {{ref('stg_matomo_actions_visits__our_page_events')}} actions
-    -- left join {{ref('stg_campaing_names_mapping__traffic_sources')}} sources
-    -- on actions.campaign_name=sources.affiliate_campaign_name
-    where  
+    left join {{ref('stg_campaign_names_mapping__traffic_sources')}} sources
+    on actions.campaign_name=sources.affiliate_campaign_name
+    --where  
         --matomo_actions.type = 'event' 
         --AND matomo_actions.subtitle = 'Category: "OutClicks, Action: "Click on casino banner"'
         --and right("right"(matomo_actions.eventname::text, length(matomo_actions.eventname::text) - 3),6)<>'sports'
@@ -22,7 +22,7 @@
         --[[ and "left"(matomo_actions.eventname::text, 2) in ( select distinct geo from campaign_names_mapping WHERE {{country_code_var}} ) ]]
         --[[ and lower(sitename) in ( select distinct console_campaign_name from campaign_names_mapping WHERE {{campaign_name_var}})]]
         --[[ and lower(sitename) in ( select distinct console_campaign_name from campaign_names_mapping WHERE {{traffic_source}})]]
-    group by campaign_name, ga_campaign_name, date_cet, brand_name, country_code--, traffic_source
+    group by campaign_name, ga_campaign_name, date_cet, brand_name, country_code, traffic_source
 )
 /*GAP campaigns aggregated data from records_gap_campaigns table*/
 union all
--- models/staging/stg_campaign_names_mapping__traffic_sources.sql
+++ models/staging/stg_campaign_names_mapping__traffic_sources.sql
@@ -2,7 +2,6 @@
 {{ config(materialized='table') }}
 
 select DISTINCT 
-    --lower(tracking_template_campaign_name) as affiliate_campaign_name, 
-    campaign_name,
+    lower(tracking_template_campaign_name) as affiliate_campaign_name, 
     traffic_source
 from {{ source('main','campaign_names_mapping') }}
