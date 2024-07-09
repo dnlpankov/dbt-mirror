441f1d5f7bcdb2b3834ea50bb6bd390ca1024419
dnlpankov <dnlpankov@gmail.com>
2024-07-09 15:13:30 +0300

    updated email_converstions_fct to properly incorporate welcome campaign conversions

--- models/marts/email_conversions_fct.sql
+++ models/marts/email_conversions_fct.sql
@@ -67,12 +67,64 @@ merged_data AS (
         AND lc.brand_name = cd.brand_name
         AND cd.date_parsed >= lc.sent_at_cet
         AND (cd.date_parsed < lc.next_sent_at_cet OR lc.next_sent_at_cet IS NULL)
-        --and cd.is_welcome_campaign is False
+        and cd.is_welcome_campaign is False
+        --and cd.first_time_deposit>0.5
     
-)
+),
 
-select * 
+updated_data as (
+    select *
+    , CASE
+        WHEN sent_at_cet IS NULL THEN true
+        WHEN (date_parsed - sent_at_cet) > interval '7 days' THEN true 
+        ELSE false 
+    END AS is_welcome_campaign_by_time_difference
+    , CASE
+        WHEN sent_at_cet IS NULL
+            THEN DATE_TRUNC('week', date_parsed)::date - interval '1 day'
+        WHEN (date_parsed - sent_at_cet) > interval '7 days' 
+            THEN DATE_TRUNC('week', date_parsed)::date - interval '1 day'
+        ELSE sent_at_cet 
+    END AS sent_at_cet_2 
 from merged_data
+where first_time_deposit>0.5
+)
+, final_data AS (
+    SELECT
+        *
+        , CASE
+            WHEN is_welcome_campaign_by_time_difference
+                THEN null
+            ELSE next_sent_at_cet
+        END AS next_sent_at_cet_2
+        , CASE
+            WHEN is_welcome_campaign_by_time_difference
+                THEN 0
+            ELSE aweber_campaign_id
+        END AS aweber_campaign_id_2
+        , CASE
+            WHEN is_welcome_campaign_by_time_difference THEN
+                'welcome_' || TO_CHAR(sent_at_cet_2, 'MM-DD-YYYY')
+            ELSE
+                'broadcast_' || TO_CHAR(sent_at_cet_2, 'MM-DD-YYYY')
+        END AS campaign_name_2
+    FROM updated_data
+)
+SELECT
+    list_id
+    , country_code
+    , brand_name
+    , campaign_name_2 as campaign_name
+    , is_welcome_campaign_by_time_difference AS is_welcome_campaign
+    , sent_at_cet_2 AS sent_at_cet
+    , next_sent_at_cet_2 AS next_sent_at_cet
+    , aweber_campaign_id_2 AS aweber_campaign_id
+    , date_parsed
+    , registrations
+    , first_time_deposit
+
+FROM final_data
+
 
 -- SELECT
 --     list_id 
--- models/staging/scraper/stg_scraper__email_records.sql
+++ models/staging/scraper/stg_scraper__email_records.sql
@@ -10,7 +10,7 @@ select
     "campaign_name"
     , "geo" as country_code
     , "brand_name"
-    --, "adgroup_name"
+    , "adgroup_name"
     , case when "adgroup_name"='trickyspins_email_welcome' then True
         else False end as "is_welcome_campaign"
     , timestamp_parsed
@@ -22,9 +22,10 @@ select
 from source
 where
     ("campaign_name" = 'email')
-    and cpa_count>0 and date_parsed>'2024-05-31'
+    --and cpa_count>0 
+    and date_parsed>'2024-05-31'
     and ("registrations" + "cpa_count" > 0) -- condition for the speed
-
+    and "cpa_count" > 0
 -- select
 --     *
 -- from source
